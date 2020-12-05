a4ed43fd5bb4b04a429fe7bbba82e59fd536422d
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 14:08:29 -0700

    add initDFC function comment

--- client.c
+++ client.c
@@ -4,6 +4,11 @@
 
 #include "client.h"
 
+/**
+ * Initialize a DFC
+ * @param fileName	Name of the configuration file.
+ * @param config	Pointer to the config to build
+ */
 void initDFC(const char *fileName, dfc *config) {
 
 }

11c5996491bcc34a5176ebfdb97ac1a1c47fad34
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 13:00:01 -0700

    creating dfc struct

--- client.h
+++ client.h
@@ -5,4 +5,13 @@
 #ifndef NETWORK_SYSTEMS_PA4_CLIENT_H
 #define NETWORK_SYSTEMS_PA4_CLIENT_H
 
+typedef struct {
+	int firstServerSocket;
+	int secondServerSocket;
+	int thirdServerSocket;
+	int fourthServerSocket;
+	char *username;
+	char *password;
+} dfc;
+
 #endif //NETWORK_SYSTEMS_PA4_CLIENT_H

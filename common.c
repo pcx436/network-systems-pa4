33583f9ef22fd6edb1b3204d882a29a5b72f6549
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:26:05 -0700

    add trimSpace to common

--- common.c
+++ common.c
@@ -3,3 +3,18 @@
 //
 
 #include "common.h"
+#include <ctype.h>
+#include <string.h>
+
+void trimSpace(char *s) {
+	int i, begin = 0, end = strlen(s) - 1;
+	while (isspace(s[end]))
+		end--;
+	s[end + 1] = '\0';
+
+	while (isspace(s[begin]))
+		begin++;
+
+	for (i = 0; i < end - 1; i++)
+		s[i] = s[i + begin];
+}
--- common.h
+++ common.h
@@ -5,4 +5,6 @@
 #ifndef NETWORK_SYSTEMS_PA4_COMMON_H
 #define NETWORK_SYSTEMS_PA4_COMMON_H
 
+void trimSpace(char *s);
+
 #endif //NETWORK_SYSTEMS_PA4_COMMON_H

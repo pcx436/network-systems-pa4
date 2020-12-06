78e7fb4853038bdef3e864433b65ac4b3babffd1
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:25:14 -0700

    remove client definitions to be used later

--- client.h
+++ client.h
@@ -4,20 +4,5 @@
 
 #ifndef NETWORK_SYSTEMS_PA4_CLIENT_H
 #define NETWORK_SYSTEMS_PA4_CLIENT_H
-#include "macro.h"
-#include <sys/socket.h>
-#include <netdb.h>
-
-typedef struct {
-	struct sockaddr_in *dfsSockets[4];
-	char username[MAX_USERNAME];
-	char password[MAX_PASSWORD];
-} dfc;
-
-int initDFC(const char *fileName, dfc *config);
-
-void destroyDFC(dfc *config);
-
-void trimSpace(char *s);
 
 #endif //NETWORK_SYSTEMS_PA4_CLIENT_H

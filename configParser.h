6e4666107041e766e4f487ad668bab1b750064aa
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:25:28 -0700

    Add client definitions to configParser

--- configParser.h
+++ configParser.h
@@ -5,4 +5,17 @@
 #ifndef NETWORK_SYSTEMS_PA4_CONFIGPARSER_H
 #define NETWORK_SYSTEMS_PA4_CONFIGPARSER_H
 
+#include "macro.h"
+#include <sys/socket.h>
+#include <netdb.h>
+
+typedef struct {
+	struct sockaddr_in *dfsSockets[4];
+	char username[MAX_USERNAME];
+	char password[MAX_PASSWORD];
+} dfc;
+
+int initDFC(const char *fileName, dfc *config);
+
+void destroyDFC(dfc *config);
 #endif //NETWORK_SYSTEMS_PA4_CONFIGPARSER_H

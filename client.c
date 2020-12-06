58522c9359227ea24f761656d08223bde45a9144
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:59:17 -0700

    add commandOption and commandWord

--- client.c
+++ client.c
@@ -10,7 +10,7 @@
 int main(int argc, const char *argv[]) {
 	dfc config;
 	int numServers;
-	char fullCommand[MAX_COMMAND], *tokenSave;
+	char fullCommand[MAX_COMMAND], *tokenSave, *commandWord, *commandOption;
 	int exit = 0;
 
 	if (argc != 2) {

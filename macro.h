92ff078b9d11be5ae0f30a46fab18f0750f28e0d
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:40:36 -0700

    Define MAX_COMMAND

--- macro.h
+++ macro.h
@@ -4,9 +4,11 @@
 
 #ifndef NETWORK_SYSTEMS_PA4_MACRO_H
 #define NETWORK_SYSTEMS_PA4_MACRO_H
+#include <linux/limits.h>
 
 #define MAX_USERNAME    1024
 #define MAX_PASSWORD    1024
 #define MAX_LINE        1024
+#define MAX_COMMAND     (PATH_MAX + 5)
 
 #endif //NETWORK_SYSTEMS_PA4_MACRO_H

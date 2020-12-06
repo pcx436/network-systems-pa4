fe4a528ae12a8e09c79c3211094372fbec5b39f4
Jacob Malcy <pcx436@yahoo.com>
2020-12-05 17:40:55 -0700

    Comments and intro

--- client.c
+++ client.c
@@ -18,9 +18,19 @@ int main(int argc, const char *argv[]) {
 		printf("Usage: %s [configuration file]", argv[0]);
 		return 1;
 	}
+
+	// failure to load dfc.conf
 	if ((numServers = initDFC(argv[1], &config)) != 4)
 		return 1;
-	
+
+	// display intro
+	printf("Welcome to Jacob's Distributed File System Client! Your available commands are:\n");
+	printf("\tget [FILE]: retrieves a file from the DFS\n\tput [FILE]: sends a file to the DFS\n\t"
+		"list: lists all available files in the DFS\n\texit: terminates the DFC\n");
+
+	do {
+
+	} while ();
 
 	return 0;
 }

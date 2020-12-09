961fc5c448449749a160cbe94c6028659c29e2c9
Jacob Malcy <pcx436@yahoo.com>
2020-12-08 17:27:02 -0700

    checking for invalid arguments

--- server.c
+++ server.c
@@ -3,8 +3,15 @@
 //
 
 #include "server.h"
+#include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+	// check for invalid arguments
+	if (argc != 2) {
+		fprintf(stderr, "ERROR: incorrect number of arguments.\n");
+		printf("Usage: %s [PORT]\n", argv[0]);
+		return 1;
+	}
 
 	return 0;
 }

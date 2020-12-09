dc1b2b6ba67b77dc8009847e3886c8e5f0fee872
Jacob Malcy <pcx436@yahoo.com>
2020-12-08 20:34:10 -0700

    fail when cannot create user directory

--- server.c
+++ server.c
@@ -208,8 +208,10 @@ int list(threadArgs tArgs, int userIndex) {
 	bzero(fileName, PATH_MAX);
 	sprintf(fileName, "%s/%s", tArgs.dir, tArgs.usernames[userIndex]);
 
-	if (stat(fileName, &st) == -1) {
-		mkdir(fileName, 700);
+	if (stat(fileName, &st) == -1 && mkdir(fileName, 700) == -1) {
+		perror("Failed to create user directory");
+		return -1;
+	}
 	}
 	else {
 		// found directory, list any files

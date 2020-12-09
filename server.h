613c167a3aba4a186a086741c74657bf1240dd9b
Jacob Malcy <pcx436@yahoo.com>
2020-12-08 18:20:24 -0700

    add function sigs and macros

--- server.h
+++ server.h
@@ -5,4 +5,12 @@
 #ifndef NETWORK_SYSTEMS_PA4_SERVER_H
 #define NETWORK_SYSTEMS_PA4_SERVER_H
 
+#define MAX_USERS   512
+#define LISTENQ     1024
+
+void *connectionHandler(void *arguments);
+int makeSocket(int port);
+void handler(int useless);
+
+static volatile int killed = 0;
 #endif //NETWORK_SYSTEMS_PA4_SERVER_H

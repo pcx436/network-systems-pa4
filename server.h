b3b9b89d1f0e50bdd97155126c00b6c2bd3c9a14
Jacob Malcy <pcx436@yahoo.com>
2020-12-08 18:29:52 -0700

    Add thread arguments struct

--- server.h
+++ server.h
@@ -4,9 +4,17 @@
 
 #ifndef NETWORK_SYSTEMS_PA4_SERVER_H
 #define NETWORK_SYSTEMS_PA4_SERVER_H
+#include <pthread.h>
 
 #define MAX_USERS   512
 #define LISTENQ     1024
+
+typedef struct {
+	int sockfd;
+	int *numThreads;
+	char *dir;
+	pthread_mutex_t *mutex;
+} threadArgs;
 
 void *connectionHandler(void *arguments);
 int makeSocket(int port);
